8bfcdb9504d39428e4aaebadd3cfc9be705cccce
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2023-02-06 00:54:01 +0200

    create

--- TableFunctions/createTable.sh
+++ TableFunctions/createTable.sh
@@ -2,33 +2,66 @@
 function CreateTable {
     typeset -i number_fields=0
     typeset -i iterator=1
-    
+    fName=""
     read -p "Enter Table Name: " Tname
     # check for regex
-    if [[ $Tname =~ ^[a-zA-Z]{1}[_0-9a-zA-Z$]{,63}$ ]]
+    if [[ $Tname =~ ^[a-zA-Z]{1}[_0-9a-zA-Z]{,31}$ ]]
     then
-        # check for existance
-        if [[ -f "$Tname" ]]; then
-            echo "Table \"$Tname\" is already exist."
+        # check that table don't end with _
+        if [[ $DBname == *[_] ]]
+        then echo "Not Valid Table name"
         else
-            read -p "Enter Number of Fields: " number_fields
-            if [ $number_fields -le 0 ]
-            then
-                echo "Number of Fields must be greater than 0"
+            # check for existance
+            if [[ -f "$Tname" ]]; then
+                echo "Table \"$Tname\" is already exist."
             else
+                read -p "Enter Number of Fields: " number_fields
+                #  it may throw 0 if user enter string
+                while [ $number_fields -le 0 ]
+                do
+                    echo "Number of Fields must be greater than 0"
+                done
+                touch "${Tname}_meta"
                 while [ $iterator -le $number_fields ];
                 do
-                    if [ $iterator -eq  1 ];
-                    then
-                        read -p "Enter Field #1 [NOTICE IT'S YOUR PK]: " fName
-                    else
-                        read -p "Enter Field #$iterator: " fName
-                    fi
-                    
+                    while [ true ]; do
+                        if [ $iterator -eq  1 ];
+                        then
+                            read -p "Enter Field #1 [NOTICE IT'S YOUR PK]: " fName
+                        else
+                            read -p "Enter Field #$iterator: " fName
+                        fi
+                        if [[ $fName =~ ^[a-zA-Z]{1}[_0-9a-zA-Z]{,31}$ ]]
+                        then
+                            # check that field don't end with _
+                            if [[ $fName == *[_] ]]
+                            then echo "Not Valid Field name"
+                            else
+                                break
+                            fi
+                        else
+                            echo "Not Valid Field name"
+                        fi
+                    done
+                    while [ true ]; do
+                        echo  "1) Integer Data Type"
+                        echo  "2) String Data Type"
+                        read -p ">>" option
+                        case $option in
+                            1) dt="-i" ; break;;
+                            2) dt="-s" ; break ;;
+                            *) flag=0; echo "not supported"
+                        esac
+                    done
+                    echo "${fName} ${dt}" >> "${Tname}_meta"
+                    iterator=$iterator+1
                 done
             fi
-            
         fi
+        touch "${Tname}"
+    else
+        echo "Not valid Table name"
+        
     fi
     ./../../menuTA.sh
 }
