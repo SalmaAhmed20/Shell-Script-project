dc8e150afc64062204778417d7fe050cecff7e8a
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2023-02-06 17:15:25 +0200

    list

--- menuTA.sh
+++ menuTA.sh
@@ -8,11 +8,11 @@ function MenuTable {
     echo "6.Delete from Table"
     echo "7.Update Table"
     echo "8.Main Menu"
-    curr=$(pwd)
-    read -p "$curr>>" option
+    curr=$(basename $(pwd))
+    read -p "DB/$curr>>" option
     case $option in
         1) echo "Create Table" ; ./../../TableFunctions/createTable.sh ;;
-        2) echo "List Tables"  ;;
+        2) echo "List Tables"  ; ./../../TableFunctions/listTable.sh ;;
         3) echo "Drop Table"  ;;
         4) echo "Insert into Table" ;;
         5) echo "Select from Table";;
