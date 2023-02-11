d5ccd93b1510430db9a443a8b41b2551ae1a5e34
SalmaAhmed20 <salma.ahmed.anees@stud.cu.eg>
2023-02-11 15:46:14 +0200

    update entire column

--- TableFunctions/updateTable.sh
+++ TableFunctions/updateTable.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
-function updateEntireColumn
-{
+function updateEntireColumn {
     meta="${1}_meta"
     datatypes=()
     tName=$1
     while [[ true ]]; do
         typeset -i idx=0
         typeset -i flag=0
-        for line in `cat $meta`
-        do
+        for line in $(cat $meta); do
             field=$line
-            if [[ flag -ne 0 ]]
-            then
-                if [[ $field != "-i" && $field != "-s" && $field != "pk" ]]
-                then
-                    echo $(($idx+1))")""$field"
+            if [[ flag -ne 0 ]]; then
+                if [[ $field != "-i" && $field != "-s" && $field != "pk" ]]; then
+                    echo $(($idx + 1))")""$field"
                     idx=$idx+1
-                    
                 else
                     datatypes=(${datatypes[@]} "${field}")
-                    echo "${datatypes[$idx]}"
+                    # echo "${datatypes[$idx]}"
                 fi
             fi
             flag=1
         done
-        echo $(($idx+1))")""Go Back"
+        echo $(($idx + 1))")""Go Back"
         idx=$idx+1
         curr=$(basename $(pwd))
         read -p "DB/$curr/${1}>>" opt
-        if [[ $opt -le 0 || $opt -gt $idx ]]
-        then
+        if [[ $opt -le 0 || $opt -gt $idx ]]; then
             echo "Not valid"
             continue
         else
@@ -37,52 +31,49 @@ function updateEntireColumn
                 echo "------cancel current updating process------"
                 break
             fi
-            if [[ ${datatypes[$opt]} = "-i" ]]
-            then
-                echo "${datatypes[$opt]}"
-                
+            echo "${datatypes[$opt]}"
+            if [[ ${datatypes[$opt]} = "-i" ]]; then
                 typeset -i data
                 read -p "update columns set " data
-                if [[ $data =~ ^[0-9]+$ ]]
-                then
-                    temp=$opt+1
-                    awk 'sub($'$opt','$data')' $tName
+                if [[ $data =~ ^[0-9]+$ ]]; then
+                    temp=$(($opt + 1))
+                    echo $temp
+                    touch .tmpupdate
+                    awk -F ":" '{sub($'$temp','$data'); print $0}' $tName >>.tmpupdate
+                    cat .tmpupdate >$tName
+                    rm .tmpupdate
                 fi
-            elif [[ ${datatypes[$temp]} = "-s" ]]
-            then
-                echo "inn2"
-                
+            elif [[ ${datatypes[$opt]} = "-s" ]]; then
+                # echo "inn2"
                 flag=0
                 read -p "update columns set " data2
-                if [[ $data2 =~ [:] ]]
-                then
+                if [[ $data2 =~ [:] ]]; then
                     echo "----Forbbiden character \":\" ----"
                 else
-                    awk '{sub($'$opt','"$data2"')}' $tName
+                    temp=$(($opt + 1))
+                    awk -F ":" '{ sub($'$temp','"\"$data2\""'); print $0}' $tName >>.tmpupdate
+                    cat .tmpupdate >$tName
+                    rm .tmpupdate
                 fi
-                
             fi
         fi
     done
 }
 function updateTable {
     read -p "Enter Table Name:  " Tname
-    if [[ $Tname =~ ^[a-zA-Z]{1}[_0-9a-zA-Z]{,31}$ ]]
-    then
-        if [[ -f $Tname  ]]
-        then
-            while [ true ]
-            do
+    if [[ $Tname =~ ^[a-zA-Z]{1}[_0-9a-zA-Z]{,31}$ ]]; then
+        if [[ -f $Tname ]]; then
+            while [ true ]; do
                 echo "1) Update Entire column"
-                echo "2) Update Specific Rows"  #delete by any column
+                echo "2) Update Specific Rows" #delete by any column
                 echo "3) Go Back"
                 curr=$(basename $(pwd))
                 read -p "DB/$curr/${Tname}>> " option
                 case $option in
-                    1) updateEntireColumn $Tname ;;
-                    2) updateSpecific $Tname ;;
-                    3) ./../../menuTA.sh;;
-                    *) echo "not supported"
+                1) updateEntireColumn $Tname ;;
+                2) updateSpecific $Tname ;;
+                3) ./../../menuTA.sh ;;
+                *) echo "not supported" ;;
                 esac
             done
         else
